b4d35f1337fa42eb08736b909d0b54faef6a740e
BR <breese@specialized-computers.com>
2018-10-11 10:36:15 -0400

    fix engine name

--- dist/js/cup.js
+++ dist/js/cup.js
@@ -15,7 +15,7 @@ var teamsx =
       [["Xiphos 0.4.2", 15], ["Nirvana 2.4", 18]],
       [["Fritz 16.10", 10], ["DeusX 1.1", 23]],
       [["Ginkgo 2.12", 7], ["Bobcat 8", 26]],
-      [["Houdini 6.03", 3], ["Chess22k 1.11", 30]],
+      [["Houdini 6.03", 3], ["chess22k 1.11", 30]],
       [["ChessBrainVB 3.70", 14], ["Arasan TCECS13.2", 19]],
       [["Jonny 8.1", 11], ["Pedone 1.9", 22]],
       [["Andscacs 094030", 6], ["Wasp 3.3", 27]]
